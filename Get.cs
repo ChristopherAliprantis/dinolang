ffcc2f596c77081c8a65e9500ea8d295e8760b73
Christopher Aliprantis <aliprantis@proton.me>
2026-03-23 10:07:07 -0700

    stuff, errors currently

---
+++ Get.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using CommandLine;
+
+namespace dinolang;
+
+public class Getfl
+{
+    [Option('m', "mode", Default = "debug", HelpText = "The mode to run in: debug or release.")]
+    public static string? Mode { get; set; }
+    [Option('f', "file", Default = null, HelpText = "The file to run.")]
+    public static string? File { get; set; }
+
+    [Option('c', "code", Default = "", HelpText = "The code to run if you dont have a file.")]
+    public static string? Code { get; set; }
+    static void Main(string[] args)
+    {
+        Parser.Default.ParseArguments<Getfl>(args)
+            .WithParsed(opt =>
+            {
+                if (opt.File != null)
+                {
+                    string code = System.IO.File.ReadAllText(opt.File);
+                }
+                else if (opt.Code != null)
+                {
+                    Console.WriteLine($"Running direct code: {opt.Code}");
+                }
+            });
+    }
+}
