70b9b874a68dc3654c062a32e8d1af0a09a08917
Christopher Aliprantis <aliprantis@proton.me>
2026-03-23 17:28:10 -0700

    added SetupStuff.cs which defines important management systems.

---
+++ src/SetupStuff.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dinolang.Interpreter
+{
+    public class Globals
+    {
+        public static Dictionary<String, Variable> Vars = new();
+        public static Dictionary<string, Function> Funcs = new();
+    }
+
+}
+
+public class Variable
+{
+    dynamic? value;
+    string? type;
+}
+
+public class Function
+{
+    List<dynamic>? parameters;
+    string? code;
+}
