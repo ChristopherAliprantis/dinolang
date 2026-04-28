1161c26d66eaa630e9ccb9f22da1069e9d4138b9
Christopher Aliprantis <aliprantis@proton.me>
2026-04-28 07:43:31 -0400

    stuff

--- src/interpreter/SetupStuff.cs
+++ src/interpreter/SetupStuff.cs
@@ -8,7 +8,6 @@ namespace dinolang.interpreter
     {
         public static Dictionary<string, Variable> Vars = new();
         public static Dictionary<string, Function> Funcs = new();
-        public static int ld = 0;
     }
 
 }
