4ffd24ccf1ad6131f7fedf3e30509f2ef21c137f
Christopher Aliprantis <aliprantis@proton.me>
2026-05-26 10:37:28 -0400

    stuff

--- src/interpreter/MainInterpreter.cs
+++ src/interpreter/MainInterpreter.cs
@@ -305,6 +305,7 @@ namespace dinolang.interpreter
             {
                 return ":";
             }
+            if (val == "SC") return ":";
             if (dinolang.interpreter.Globals.Vars.ContainsKey(val))
             {
                 return dinolang.interpreter.Globals.Vars[val].value;
