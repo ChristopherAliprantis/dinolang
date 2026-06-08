63cfd74f7445da9bdc2aa96a665da877891d749d
Christopher Aliprantis <aliprantis@proton.me>
2026-06-08 09:18:47 -0400

    changed the way Dino reads the text

--- src/Get.cs
+++ src/Get.cs
@@ -30,7 +30,7 @@ public partial class GetCode
         string TfilePath = Path.Combine(baseDir, "interpreter", "Time.dno");
         string OfilePath = Path.Combine(baseDir, "interpreter", "Output.dno");
 
-        string code = System.IO.File.ReadAllText(SfilePath, Encoding.UTF8) + System.IO.File.ReadAllText(MfilePath, Encoding.UTF8) + System.IO.File.ReadAllText(IfilePath, Encoding.UTF8) + System.IO.File.ReadAllText(OfilePath, Encoding.UTF8) + System.IO.File.ReadAllText(TfilePath, Encoding.UTF8);
+        string code = System.IO.File.ReadAllText(SfilePath, Encoding.UTF8).Replace("\r\n", "").Replace("\r", "").Replace("\n", "") + System.IO.File.ReadAllText(MfilePath, Encoding.UTF8).Replace("\r\n", "").Replace("\r", "").Replace("\n", "") + System.IO.File.ReadAllText(IfilePath, Encoding.UTF8).Replace("\r\n", "").Replace("\r", "").Replace("\n", "") + System.IO.File.ReadAllText(OfilePath, Encoding.UTF8).Replace("\r\n", "").Replace("\r", "").Replace("\n", "") + System.IO.File.ReadAllText(TfilePath, Encoding.UTF8).Replace("\r\n", "").Replace("\r", "").Replace("\n", "");
         Parser.Default.ParseArguments<GetCode>(args)
             .WithParsed(opt =>
             {
