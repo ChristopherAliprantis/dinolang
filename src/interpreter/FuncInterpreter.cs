6e80f5fe1266e9c37f46bba49926e09f14ae1072
Christopher Aliprantis <aliprantis@proton.me>
2026-07-02 13:08:17 +0300

    bug fix and removed neg(num); function

--- src/interpreter/FuncInterpreter.cs
+++ src/interpreter/FuncInterpreter.cs
@@ -178,7 +178,6 @@ namespace dinolang.interpreter
                     }
                     dynamic? thing = (7, 7);
                     if (COND == true) thing = ProcessIf(IfLines, true, false);
-
                     if (thing is System.ValueTuple<int, int>)
                     {
 
