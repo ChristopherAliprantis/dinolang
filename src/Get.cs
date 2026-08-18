efcef1cd300bf24964f57991185af5374bbff2aa
Christopher Aliprantis <aliprantis@proton.me>
2026-08-18 07:09:10 -0700

    stuff

--- src/Get.cs
+++ src/Get.cs
@@ -30,7 +30,7 @@ public partial class GetCode
         string TfilePath = Path.Combine(baseDir, "interpreter", "Time.dno");
         string OfilePath = Path.Combine(baseDir, "interpreter", "Output.dno");
         string BfilePath = Path.Combine(baseDir, "interpreter", "Bool.dno");
-        string LfilePath = Path.Combine(baseDir, "interpreter", "Lists.dno");
+        string CfilePath = Path.Combine(baseDir, "interpreter", "Collections.dno");
 
         string code = System.IO.File.ReadAllText(SfilePath, Encoding.UTF8).Replace("\r", "").Replace("\n", "")
                     + System.IO.File.ReadAllText(MfilePath, Encoding.UTF8).Replace("\r", "").Replace("\n", "")
@@ -38,7 +38,7 @@ public partial class GetCode
                     + System.IO.File.ReadAllText(IfilePath, Encoding.UTF8).Replace("\r", "").Replace("\n", "")
                     + System.IO.File.ReadAllText(OfilePath, Encoding.UTF8).Replace("\r", "").Replace("\n", "")
                     + System.IO.File.ReadAllText(TfilePath, Encoding.UTF8).Replace("\r", "").Replace("\n", "")
-                    + System.IO.File.ReadAllText(LfilePath, Encoding.UTF8).Replace("\r", "").Replace("\n", "");
+                    + System.IO.File.ReadAllText(CfilePath, Encoding.UTF8).Replace("\r", "").Replace("\n", "");
         if (args.Length == 0)
         {
             Console.WriteLine("Dino Interactive Environment"+ Environment.NewLine);
