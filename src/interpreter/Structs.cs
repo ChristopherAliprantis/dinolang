f0bf757b085ae7bf2d4602e62a9865618926f2a0
Christopher Aliprantis <aliprantis@proton.me>
2026-09-06 08:09:46 -0700

    stuff

--- src/interpreter/Structs.cs
+++ src/interpreter/Structs.cs
@@ -24,17 +24,7 @@ public struct Struct
         foreach (var field in blueprint.fields)
         {
             string f = instancevarname + "." + field;
-            if (f == instancevarname)
-            {
-                Console.WriteLine($"Cannot assign field name to instance variable Line {line} Try going on https://github.com/ChristopherAliprantis/dinolang/wiki/ for help");
-                Environment.Exit(1);
-            }
-            else if (f == "TextBGColor" || f == "DLine" || f == "NL" || f == "BLANK" || f == "COMMA" || f == "COLON" || f == "SC")
-            {
-                Console.WriteLine($"Invalid Value {f}, Line {line} Try going on https://github.com/ChristopherAliprantis/dinolang/wiki/ for help");
-                Environment.Exit(1);
-            }
-            else if (!Globals.Vars.ContainsKey(f))
+            if (!Globals.Vars.ContainsKey(f))
             {
                 Globals.Vars[f] = new Variable
                 {
